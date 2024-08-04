8f69dd328511f14dc8a296f9b3b6a62888ac41c5
buraksenyurt <burakselimsenyurt@gmail.com>
2024-08-04 15:25:52 +0300

    Added simple iterations.

--- simple_ecs/src/main.rs
+++ simple_ecs/src/main.rs
@@ -30,7 +30,29 @@ impl World {
 }
 fn main() {
     let mut game_world = World::new();
-    game_world.create_entity(Some(Health(40)), Some(Attack(5)), Some(Id("Azurat")));
+    game_world.create_entity(Some(Health(-5)), Some(Attack(5)), Some(Id("Mithrandir")));
     game_world.create_entity(Some(Health(80)), Some(Attack(2)), Some(Id("Nazbendiyr")));
-    game_world.create_entity(Some(Health(1000)), None, Some(Id("CrakenGate")))
+    game_world.create_entity(Some(Health(1000)), None, Some(Id("Tower_1")));
+    game_world.create_entity(Some(Health(50)), None, Some(Id("Artillary_1")));
+    game_world.create_entity(Some(Health(-1)), Some(Attack(1)), Some(Id("Villager_Jonas")));
+
+    let attackers = game_world
+        .healths
+        .iter()
+        .zip(game_world.attacks.iter())
+        .zip(game_world.ids.iter());
+
+    let attackers_with_ids = attackers.filter_map(
+        |((health, attack), id): ((&Option<Health>, &Option<Attack>), &Option<Id>)| {
+            Some((health.as_ref()?, attack.as_ref()?, id.as_ref()?))
+        },
+    );
+
+    for (h, a, n) in attackers_with_ids {
+        if h.0 < 0 {
+            println!("{} has been dead!", n.0);
+        } else {
+            println!("{} is still healthy and has {} attack power", n.0, a.0);
+        }
+    }
 }
