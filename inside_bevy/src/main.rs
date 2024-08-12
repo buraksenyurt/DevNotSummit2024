8809516ed2979cf147a32a84f7df3d33da0a8853
buraksenyurt <burakselimsenyurt@gmail.com>
2024-08-12 20:37:21 +0300

    Refactored code.

--- inside_bevy/src/main.rs
+++ inside_bevy/src/main.rs
@@ -21,13 +21,28 @@ fn main() {
     let mut enemy = world.spawn_empty();
     enemy.insert((Position { x: 50.0, y: 0.0 }, Velocity { x: -1.0, y: 1.0 }));
 
+    let mut tower = world.spawn_empty();
+    tower.insert(Position { x: 25.0, y: 25.0 });
+
     let mut schedule = Schedule::default();
-    schedule.add_systems(show_actors);
+    schedule.add_systems(visit_positions);
+    schedule.run(&mut world);
+
+    println!();
+
+    schedule.add_systems(move_actors);
     schedule.run(&mut world);
 }
 
-fn show_actors(query: Query<&Position>) {
-    for p in &query {
-        println!("Position of entity {:?}", p);
+fn visit_positions(query: Query<(Entity, &Position)>) {
+    for (entity, position) in query.iter() {
+        println!("{:?}\t{:?}. ", entity, position);
+    }
+}
+
+fn move_actors(mut query: Query<(&mut Position, &Velocity)>) {
+    for (mut position, velocity) in query.iter_mut() {
+        position.x += velocity.x;
+        position.y += velocity.y;
     }
 }
