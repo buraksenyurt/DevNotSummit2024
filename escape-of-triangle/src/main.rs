8a2dbc6af252206e8dbf0f6208a8d82c8a3b2680
buraksenyurt <burakselimsenyurt@gmail.com>
2024-08-17 16:02:49 +0300

    Added player entity.

--- escape-of-triangle/src/main.rs
+++ escape-of-triangle/src/main.rs
@@ -3,12 +3,22 @@ mod constants;
 use crate::constants::*;
 use bevy::prelude::*;
 use bevy::sprite::MaterialMesh2dBundle;
+use rand::Rng;
 
 #[derive(Component)]
 struct Position(Vec2);
 
 #[derive(Component)]
+struct Velocity(Vec2);
+
+#[derive(Component)]
+struct Direction(Vec2);
+
+#[derive(Component)]
 struct Tower;
+
+#[derive(Component)]
+struct Player;
 
 #[derive(Bundle)]
 struct TowerBundle {
@@ -22,6 +32,51 @@ impl TowerBundle {
             tower: Tower,
             position: Position(Vec2::new(x, y)),
         }
+    }
+}
+
+#[derive(Bundle)]
+struct PlayerBundle {
+    player: Player,
+    direction: Direction,
+    position: Position,
+    velocity: Velocity,
+}
+
+impl PlayerBundle {
+    fn new(x: f32, y: f32) -> Self {
+        Self {
+            player: Player,
+            direction: Direction(Vec2::ZERO),
+            position: Position(Vec2::new(x, y)),
+            velocity: Velocity(Vec2::ZERO),
+        }
+    }
+}
+fn spawn_player(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    window: Query<&Window>,
+) {
+    if let Ok(window) = window.get_single() {
+        let w_height = window.resolution.height();
+        let w_width = window.resolution.width();
+
+        let shape = Mesh::from(RegularPolygon::new(TRIANGLE_CIRCUS_RADIUS, 3));
+        let color = ColorMaterial::from(TRIANGLE_COLOR);
+
+        let mesh_handle = meshes.add(shape);
+        let material_handle = materials.add(color);
+
+        commands.spawn((
+            PlayerBundle::new(w_width * 0.25, w_height * 0.25),
+            MaterialMesh2dBundle {
+                mesh: mesh_handle.into(),
+                material: material_handle,
+                ..default()
+            },
+        ));
     }
 }
 
@@ -44,7 +99,7 @@ fn spawn_towers(
 
         for position in positions.iter() {
             let shape = Mesh::from(Rectangle::new(TOWER_WIDTH, TOWER_HEIGHT));
-            let color = ColorMaterial::from(Color::srgb(0.8, 0.4, 0.4));
+            let color = ColorMaterial::from(TOWER_COLOR);
 
             let mesh_handle = meshes.add(shape);
             let material_handle = materials.add(color);
@@ -64,8 +119,8 @@ fn spawn_towers(
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (spawn_towers, spawn_camera))
-        .add_systems(Update, draw_towers)
+        .add_systems(Startup, (spawn_towers, spawn_camera, spawn_player))
+        .add_systems(Update, (draw_towers, draw_player))
         .run();
 }
 
@@ -74,6 +129,12 @@ fn spawn_camera(mut commands: Commands) {
 }
 
 fn draw_towers(mut positions: Query<(&mut Transform, &Position), With<Tower>>) {
+    for (mut transform, position) in &mut positions {
+        transform.translation = position.0.extend(0.);
+    }
+}
+
+fn draw_player(mut positions: Query<(&mut Transform, &Position), With<Player>>) {
     for (mut transform, position) in &mut positions {
         transform.translation = position.0.extend(0.);
     }
--- pong/src/constants.rs
+++
@@ -1,2 +0,0 @@
-pub const TOWER_WIDTH: f32 = 64.;
-pub const TOWER_HEIGHT: f32 = 64.;
