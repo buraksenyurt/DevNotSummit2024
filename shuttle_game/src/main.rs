ff594b854251794c166d0019dc858afa408e0a6a
buraksenyurt <burakselimsenyurt@gmail.com>
2024-08-25 01:36:31 +0300

    Moved to plugins.

--- shuttle_game/src/main.rs
+++ shuttle_game/src/main.rs
@@ -1,38 +1,57 @@
 use bevy::prelude::*;
 
 #[derive(Component, Debug)]
-struct Position {
-    x: f32,
-    y: f32,
-}
-
-#[derive(Component, Debug)]
 struct Velocity {
-    x: f32,
-    y: f32,
+    pub value: Vec3,
 }
 
-fn spawn_shuttle(mut commands: Commands) {
-    commands.spawn((Position { x: 0., y: 0. }, Velocity { x: 1., y: 1. }));
-}
+pub struct ShuttlePlugin;
 
-fn update_position(mut query: Query<(&mut Position, &Velocity)>) {
-    for (mut pos, vel) in query.iter_mut() {
-        pos.x += vel.x;
-        pos.y += vel.y;
+impl Plugin for ShuttlePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_shuttle);
     }
 }
 
-fn log_position(query: Query<(Entity, &Position)>) {
-    for (entity, pos) in query.iter() {
-        info!("Entity {:?} at {:?}", entity, pos);
+fn spawn_shuttle(mut commands: Commands) {
+    commands.spawn((SpatialBundle::default(), Velocity { value: Vec3::ZERO }));
+}
+
+pub struct MovementPlugin;
+
+impl Plugin for MovementPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, update_position);
+    }
+}
+
+pub struct InfoPlugin;
+
+impl Plugin for InfoPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, log_position);
+    }
+}
+
+fn update_position(mut query: Query<(&mut Transform, &Velocity)>) {
+    for (mut transform, vel) in query.iter_mut() {
+        transform.translation.x += vel.value.x;
+        transform.translation.y += vel.value.y;
+        transform.translation.z += vel.value.z;
+    }
+}
+
+fn log_position(query: Query<(Entity, &Transform)>) {
+    for (entity, transform) in query.iter() {
+        info!("Entity {:?} at {:?}", entity, transform.translation);
     }
 }
 
 fn main() {
     App::new()
-        .add_systems(Startup, spawn_shuttle)
-        .add_systems(Update, (update_position, log_position))
+        .add_plugins(ShuttlePlugin)
+        .add_plugins(MovementPlugin)
+        .add_plugins(InfoPlugin)
         .add_plugins(DefaultPlugins)
         .run();
 }
