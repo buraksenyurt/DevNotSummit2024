fa318f941632a8330ab302d0a9087191023a5caa
buraksenyurt <burakselimsenyurt@gmail.com>
2024-08-25 01:02:18 +0300

    Refactored.

--- escape-of-triangle/src/constants.rs
+++
@@ -1,7 +0,0 @@
-use bevy::color::Color;
-
-pub const TOWER_WIDTH: f32 = 64.;
-pub const TOWER_HEIGHT: f32 = 64.;
-pub const TOWER_COLOR: Color = Color::srgb(1., 0.70, 0.72);
-pub const TRIANGLE_COLOR: Color = Color::srgb(1., 1., 0.729);
-pub const TRIANGLE_CIRCUS_RADIUS: f32 = 20.;
