4ac758d84a1561763dcc62c07dc9f22791fcbe6e
buraksenyurt <burakselimsenyurt@gmail.com>
2024-08-25 16:25:01 +0300

    Added acceleration based movement.

--- shuttle_game/src/shuttle.rs
+++ shuttle_game/src/shuttle.rs
@@ -1,16 +1,10 @@
-use crate::movement::Velocity;
+use crate::movement::{Acceleration, MovingObjectBundle, Velocity};
 use bevy::app::{App, Plugin, Startup};
 use bevy::math::Vec3;
 use bevy::prelude::*;
 
 const INITIAL_POSITION: Vec3 = Vec3::new(0.0, 0.0, 0.0);
 const VELOCITY: Vec3 = Vec3::new(0.0, 0.0, 1.0);
-
-#[derive(Bundle)]
-struct ShuttleBundle {
-    velocity: Velocity,
-    model: SceneBundle,
-}
 
 pub struct ShuttlePlugin;
 impl Plugin for ShuttlePlugin {
@@ -19,8 +13,9 @@ impl Plugin for ShuttlePlugin {
     }
 }
 fn spawn_shuttle(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(ShuttleBundle {
+    commands.spawn(MovingObjectBundle {
         velocity: Velocity { value: VELOCITY },
+        acceleration: Acceleration::new(Vec3::ZERO),
         model: SceneBundle {
             scene: asset_server.load("shuttle.glb#Scene0"),
             transform: Transform::from_translation(INITIAL_POSITION),
