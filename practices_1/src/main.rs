d156ee3e451e92efe5807db38269a23de3bea2c4
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2024-09-13 07:00:32 +0000

    Added some systems.

--- practices_1/src/main.rs
+++ practices_1/src/main.rs
@@ -1,12 +1,13 @@
 use bevy::prelude::*;
+use rand::Rng;
 
-#[derive(Component, Debug)]
+#[derive(Component, Default, Debug)]
 struct Position {
     pub x: i32,
     pub y: i32,
 }
 
-#[derive(Component, Debug)]
+#[derive(Component, Default, Debug)]
 struct Velocity {
     pub x: i32,
     pub y: i32,
@@ -14,9 +15,51 @@ struct Velocity {
 
 #[derive(Component, Debug)]
 struct Actor {
-    name: String,
+    pub name: String,
 }
 
 fn main() {
-    let world = World::new();
+    let mut world = World::new();
+
+    let mut warrior = world.spawn_empty();
+    warrior.insert((
+        Actor {
+            name: "Durin".to_string(),
+        },
+        Position::default(),
+        Velocity { x: 10, y: 10 },
+    ));
+
+    let mut legolas = world.spawn_empty();
+    legolas.insert((
+        Actor {
+            name: "Legolas".to_string(),
+        },
+        Position::default(),
+        Velocity { x: 20, y: 20 },
+    ));
+
+    let mut tower = world.spawn_empty();
+    tower.insert(Position::default());
+
+    let mut runner = Schedule::default();
+
+    runner.add_systems(locate_all.after(movement));
+
+    runner.run(&mut world);
+}
+
+fn locate_all(mut query: Query<&mut Position>) {
+    let mut rnd = rand::thread_rng();
+    for mut p in query.iter_mut() {
+        p.x = rnd.gen_range(0..100);
+        p.y = 0;
+    }
+}
+
+fn movement(mut query: Query<(&mut Position, &Velocity)>) {
+    for (mut p, v) in query.iter_mut() {
+        p.x += v.x;
+        p.y += v.y;
+    }
 }
